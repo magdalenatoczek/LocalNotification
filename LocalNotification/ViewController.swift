a84af74045f5fc640a200a8944eaaec856d19a17
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-18 08:52:11 +0200

    Initial Commit

---
+++ LocalNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNotification
+//
+//  Created by Magdalena Toczek on 18/09/2020.
+//  Copyright © 2020 Popeq Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
