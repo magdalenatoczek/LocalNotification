b176ca0e2948da8f6e3f75ffb417a17a8601073e
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-18 09:56:19 +0200

    user notification service basic set up

--- LocalNotification/Service/UserNotificationService.swift
+++ LocalNotification/Service/UserNotificationService.swift
@@ -12,11 +12,66 @@ import UserNotifications
 
 class UserNotificationService : NSObject {
     
+    private override init() {}
     
+    static let INSTANCE = UserNotificationService()
+    
+    let unCenter = UNUserNotificationCenter.current()
+    
+    func autorize(){
+        
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        unCenter.requestAuthorization(options: options) { (accessGranted, error) in
+            
+            guard error == nil else{
+                //display error
+               debugPrint("error requesting Autorization (User Notification)")
+                return
+            }
+            
+            guard accessGranted else {
+                debugPrint("user denied access - (User Notification)")
+                 //Handle user denying permissions..
+                return
+            }
+            
+            self.configure()
+             debugPrint("user accept notification")
+        }
+        
+    }
+    
+    
+    func configure(){
+        unCenter.delegate = self
+        
+    }
+    
+}
+
+
+extension UserNotificationService : UNUserNotificationCenterDelegate {
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        debugPrint("User Notification did recive response")
+        //user tapped notification
+        completionHandler()
+    }
+    
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        debugPrint("User Notification will present")
+        // foreground what should happen - different oprions
+ 
+        let options: UNNotificationPresentationOptions = [.alert, .sound]
+        completionHandler(options)
+    }
     
     
     
 }
-
 
 
