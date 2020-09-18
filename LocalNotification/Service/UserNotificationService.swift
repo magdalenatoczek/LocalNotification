5e0e576eee5011795a57006c815e96cbe9c3011d
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-18 09:11:44 +0200

    init

---
+++ LocalNotification/Service/UserNotificationService.swift
@@ -0,0 +1,22 @@
+//
+//  UserNotificationService.swift
+//  LocalNotification
+//
+//  Created by Magdalena Toczek on 18/09/2020.
+//  Copyright © 2020 Popeq Apps. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+
+class UserNotificationService : NSObject {
+    
+    
+    
+    
+    
+}
+
+
+
