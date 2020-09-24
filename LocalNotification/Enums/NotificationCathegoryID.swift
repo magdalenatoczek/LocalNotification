db6ab7b66e456a0f4db875a255b2629f8c996311
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-24 17:39:33 +0200

    category id enum

---
+++ LocalNotification/Enums/NotificationCathegoryID.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationCathegoryID.swift
+//  LocalNotification
+//
+//  Created by Magdalena Toczek on 24/09/2020.
+//  Copyright © 2020 Popeq Apps. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationCategoryID : String{
+    case timer = "notification.cathegory.timer"
+    case calendar = "notification.cathegory.data"
+    case location = "notification.cathegory.location"
+}
